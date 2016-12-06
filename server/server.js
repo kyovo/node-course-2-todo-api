440887172b47053da0f16edfeba5f54d8399b350
Kyovo Digaw <kyovodigaw@gmail.com>
2016-12-06 18:48:26 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mangoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,28 @@ app.get('/todos', (req, res) => {
   },(e)=> {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res)=>{
+  var id = req.params.id;
+
+  //Valid id using isValid
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+
 });
 
 app.listen(3000, ()=> {
