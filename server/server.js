5619d39b30f93c96d16db75109350af7c020af67
Kyovo Digaw <kyovodigaw@gmail.com>
2016-12-19 13:09:34 +0100

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -8,8 +8,9 @@ const {ObjectID} = require('mongodb');
 var {mangoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
 var app = express();
+
+
 //for heroku
 const port = process.env.PORT;
 
@@ -104,6 +105,19 @@ app.patch('/todos/:id', (req, res) =>{
   }).catch((e) => {
     res.status(400).send();
   });
+});
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
 });
 
 app.listen(port, ()=> {
